03b63324fbb4f0db46c84685299782ceda4ed21f
rrtechmart <113099947+rrtechmart@users.noreply.github.com>
2022-11-04 07:47:41 +0600

    understanding how to check prime number
    practicing how to find out the the prime number by java script

---
+++ checkPrime.js
@@ -0,0 +1,13 @@
+var n = 79;
+for( i = 2; i < n; i++){
+    if(n % i == 0){
+        console.log("Your number is not prime")
+        return;
+    }
+    else{
+        console.log("Your number is  prime")
+        return;
+    }
+}
+
+
