e83eee99ac9ecc13a87523029d1e7db9a2043bce
rrtechmart <113099947+rrtechmart@users.noreply.github.com>
2022-11-05 19:21:28 +0600

    I added inchtofeet file in github
    I added it to keep in safe store

---
+++ inchToFeet.js
@@ -0,0 +1,11 @@
+function inchToFeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+var dadaFeet = inchToFeet(108);
+console.log(dadaFeet);
+var dadiFeet = inchToFeet(156);
+console.log(dadiFeet);
+var nanaFeet = inchToFeet(168);
+console.log(nanaFeet);
+
